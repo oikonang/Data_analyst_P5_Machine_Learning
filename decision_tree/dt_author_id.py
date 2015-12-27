fef22ec19c731ddcfc1e9a9f5d0716ff3dbda7ac
oikonang <angelos.ikonomakis@gmail.com>
2015-12-27 18:51:01 +0200

    end of mini-Project 3. Percentile changed from 10 to 1

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+print "Number of features in the data: ", len(features_train[0])
 
+from sklearn.metrics import accuracy_score
+acc_min_samples_split_40 = accuracy_score(pred, labels_test)
+
+def submitAccuracies():
+	print "acc_min_samples_split_40: ", round(acc_min_samples_split_40, 3)
+
+submitAccuracies()
 #########################################################
 
 
