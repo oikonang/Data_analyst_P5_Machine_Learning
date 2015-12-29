1186e4f452ee986a70f812a29cbb04cc575435e0
oikonang <angelos.ikonomakis@gmail.com>
2015-12-29 21:02:34 +0200

    half-end of lesson 5. Count poi and count_poi_from_text created.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,27 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+keys = enron_data.keys()
+features_dict = enron_data.values()
+
+def count_poi():
+	count = 0
+	for person in enron_data:
+		if enron_data[person]['poi'] == True:
+				count = count + 1
+	return count
+
+def count_poi_from_text():
+	poi_names = open('../final_project/poi_names.txt', 'r')
+	a = poi_names.read()
+
+	for count, line in enumerate(a):
+		return enumerate(a)
+
+print "No. of persons: %d" % len(keys)
+print "No. of variables for each person: %d" % len(features_dict[0])
+#print "No. of pois in the dataset: %d" % sum( [ enron_data[p]['poi'] for p in enron_data ] )
+print "No. of pois in the dataset: ", count_poi()
+print "No. of pois in the text: ", count_poi_from_text()
+
 
