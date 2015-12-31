6fd73507df06287fdb6ac1807fe522a9fa110de1
oikonang <angelos.ikonomakis@gmail.com>
2015-12-31 12:56:17 +0200

    end of lesson's five obligatory quizes

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -34,12 +34,43 @@ def count_poi_from_text():
 	a = poi_names.read()
 
 	for count, line in enumerate(a):
-		return enumerate(a)
+		return count
+
+def max_total_payments_person():
+	sjk = enron_data['SKILLING JEFFREY K']['total_payments']
+	lkl = enron_data['LAY KENNETH L']['total_payments']
+	fas = enron_data['FASTOW ANDREW S']['total_payments']
+	max_of_three = max([sjk, lkl, fas])
+	for p in enron_data:
+		if enron_data[p]['total_payments'] == max_of_three:
+			return p
+
+def no_of_salaries():
+	count = 0
+	for p in enron_data:
+		if enron_data[p]['salary'] != 'NaN':
+			count = count + 1
+	return count
+
+def no_of_emails():
+	count = 0
+	for p in enron_data:
+		if enron_data[p]['email_address'] != 'NaN':
+			count = count + 1
+	return count
+
 
 print "No. of persons: %d" % len(keys)
 print "No. of variables for each person: %d" % len(features_dict[0])
 #print "No. of pois in the dataset: %d" % sum( [ enron_data[p]['poi'] for p in enron_data ] )
 print "No. of pois in the dataset: ", count_poi()
 print "No. of pois in the text: ", count_poi_from_text()
-
-
+print "Total value of stock belonging to James Prentice: ", enron_data['PRENTICE JAMES']['total_stock_value']
+print "No. of emails to poi by Wesley Colwell: ", enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+print "Stock options exercised by Jeffrey Skilling: ", enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+print "Total payments of Jeffrey Skilling: ", enron_data['SKILLING JEFFREY K']['total_payments']
+print "Total payments of Kenneth Lay: ", enron_data['LAY KENNETH L']['total_payments']
+print "Total payments of Andrew Fastow: ", enron_data['FASTOW ANDREW S']['total_payments']
+print "Max total payments person between the top 3 executives: ", max_total_payments_person()
+print "No. of persons that have a quantified salary:", no_of_salaries()
+print "No. of persons that have known emails:", no_of_emails()
